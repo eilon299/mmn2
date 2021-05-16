aaacf4967c8128a110c2ab7b7911966396545d94
eilon299 <eilonkornboim@gmail.com>
2021-05-16 15:13:21 +0300

    crude API - skeleton plus

--- sol.py
+++ sol.py
@@ -7,57 +7,136 @@ from Business.RAM import RAM
 from Business.Disk import Disk
 from psycopg2 import sql
 
+class SQLRet:
+    def __init__(self, ret_val=None, rows_affected=None, result=None):
+        self.ret_val = ret_val
+        self.rows_affected = rows_affected
+        self.result = result
 
-def createTables():
-    pass
+def sql_command(query, printSchema=False):
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        rows_effected, result = conn.execute(query, printSchema)
+        conn.commit()
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+        return ReturnValue.ERROR
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+        return ReturnValue.ERROR
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+        return ReturnValue.ERROR
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+        return ReturnValue.ERROR
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+        return ReturnValue.ERROR
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after try termination or exception handling
+        conn.close()
+        return SQLRet(ReturnValue.OK, rows_effected, result)
 
 
-def clearTables():
-    pass
+def insert(table, obj):
+    # TODO - SQL action to add the query ↓
+    ret = sql_command("INSERT INTO {} values {}".format(table, tuple(obj)))
+
+    if ret.rows_affected == 0:  # lookup queryID to check that it's unique and not in DB yet
+        return ReturnValue.ALREADY_EXISTS
+    else:
+        return ret.ret_val
 
 
-def dropTables():
-    pass
+def createTables():  # TODO - check this SQL code ↓ and also reduce to a single query (maybe using group?)
+    sql_command("CREATE TABLE TQuery(queryID INTEGER PRIMARY KEY, purpose TEXT NOT NULL, size INTEGER NOT NULL)")
+    sql_command("CREATE TABLE TRAM(ramID INTEGER PRIMARY KEY, company TEXT NOT NULL, size INTEGER NOT NULL)")
+    sql_command(
+        "CREATE TABLE TDisk(diskID INTEGER PRIMARY KEY, company TEXT NOT NULL, speed INTEGER NOT NULL, free_space INTEGER NOT NULL, cost INTEGER NOT NULL)")
+
+
+def clearTables():  # TODO - check this SQL code ↓ and also reduce to a single query (maybe using group?)
+    sql_command("DELETE FROM TQuery WHERE true")
+    sql_command("DELETE FROM TRAM WHERE true")
+    sql_command("DELETE FROM TDisk WHERE true")
+
+
+def dropTables():  # TODO - check this SQL code ↓ and also reduce to a single query (maybe using grouping?)
+    sql_command("DROP TABLE IF EXISTS TQuery CASCADE")
+    sql_command("DROP TABLE IF EXISTS TRAM CASCADE")
+    sql_command("DROP TABLE IF EXISTS TDisk CASCADE")
 
 
 def addQuery(query: Query) -> ReturnValue:
-    return ReturnValue.OK
+    if query.getQueryID() is None or query.getPurpose() is None or query.getSize() is None or \
+            query.getQueryID() < 1 or query.getSize() < 0:
+        return ReturnValue.BAD_PARAMS
+
+    return insert("TQuery", query)
 
 
 def getQueryProfile(queryID: int) -> Query:
-    return Query()
+    ret = sql_command("SELECT * FROM TQuery WHERE TQuery.queryID = queryID")
+    if ret.result is not None:
+        return ret.result
+    else:
+        return Query.badQuery()
 
 
 def deleteQuery(query: Query) -> ReturnValue:
-    return ReturnValue.OK
+    ret = sql_command("DELETE * FROM TQuery WHERE TQuery.queryID = queryID")
+    return ret.ret_val
 
 
 def addDisk(disk: Disk) -> ReturnValue:
-    return ReturnValue.OK
+    if disk.getDiskID() is None or disk.getCompany() is None or disk.getSpeed() is None or disk.getFreeSpace() is None or disk.getCost() is None \
+            or disk.getDiskID() < 1 or disk.getSpeed() < 1 or disk.getCost() < 1 or disk.getFreeSpace() < 0:
+        return ReturnValue.BAD_PARAMS
+
+    return insert("TDisk", disk)
+
 
 
 def getDiskProfile(diskID: int) -> Disk:
-    return Disk()
+    ret = sql_command("SELECT * FROM TDisk WHERE TDisk.diskID = diskID")
+    if ret.result is not None:
+        return ret.result
+    else:
+        return Disk.badDisk()
 
 
 def deleteDisk(diskID: int) -> ReturnValue:
-    return ReturnValue.OK
+    ret = sql_command("DELETE * FROM TDisk WHERE TDisk.diskID = diskID")
+    return ret.ret_val
 
 
 def addRAM(ram: RAM) -> ReturnValue:
-    return ReturnValue.OK
+    if ram.getRamID() is None or ram.getCompany() is None or ram.getSize() is None or ram.getRamID() < 1 or ram.getSize() < 1:
+        return ReturnValue.BAD_PARAMS
+
+    return insert("TRAM", ram)
 
 
 def getRAMProfile(ramID: int) -> RAM:
-    return RAM()
+    ret = sql_command("SELECT * FROM TRAM WHERE TRAM.ramID = ramID")
+    if ret.result is not None:
+        return ret.result
+    else:
+        return RAM.badRAM()
 
 
 def deleteRAM(ramID: int) -> ReturnValue:
-    return ReturnValue.OK
+    ret = sql_command("DELETE * FROM TRAM WHERE TRAM.ramID = ramID")
+    return ret.ret_val
 
 
 def addDiskAndQuery(disk: Disk, query: Query) -> ReturnValue:
-    return ReturnValue.OK
+
+    return ReturnValue.OK #TODO - need in 1 Q to assure both actions will succeed and also do them
 
 
 def addQueryToDisk(query: Query, diskID: int) -> ReturnValue:
